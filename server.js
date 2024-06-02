2e8bcdc9d339659b1842ad74f124c46fc92b66f5
Robin Snijder <91336310+Robskan@users.noreply.github.com>
2024-06-02 19:49:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -11,46 +10,11 @@ const redirects = {
     // Add more redirects as needed
 };
 
-// Discord webhook URL
-const discordWebhookURL = 'https://discord.com/api/webhooks/1246878907894861929/laQIVR5taSn3PomULkupTyEfBPDIHigYryZmAY-pKo7qQiTEY36GkM6TrnFN09z0FciO';
-
-// Middleware to log all requests and send to Discord
-app.use((req, res, next) => {
-    const requestPath = req.path.toLowerCase();
-    
-    // Prepare the payload for the webhook
-    const payload = {
-        content: `Page visited: ${requestPath}`
-    };
-
-    // Log the payload
-    console.log('Payload:', payload);
-
-    // Function to send the webhook request with retries
-    const sendWebhook = async (retries = 3) => {
-        try {
-            const response = await axios.post(discordWebhookURL, payload);
-            console.log('Webhook sent successfully:', response.data);
-        } catch (error) {
-            console.error('Error sending webhook:', error.response ? error.response.data : error.message);
-            if (retries > 0) {
-                console.log(`Retrying... Attempts left: ${retries}`);
-                setTimeout(() => sendWebhook(retries - 1), 1000); // Retry after 1 second
-            }
-        }
-    };
-
-    // Send the webhook
-    sendWebhook();
-
-    next();
-});
-
 // Middleware to handle redirects for /r/ paths
 app.use((req, res, next) => {
-    const requestPath = req.path.toLowerCase();
-    if (requestPath.startsWith('/r/')) {
-        const redirectTo = redirects[requestPath];
+    const path = req.path.toLowerCase();
+    if (path.startsWith('/r/')) {
+        const redirectTo = redirects[path];
         if (redirectTo) {
             return res.redirect(301, redirectTo);
         } else {
